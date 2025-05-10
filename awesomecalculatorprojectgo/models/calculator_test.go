38cfd1f8d82285a475fc4c104dabf0adbed4bb59
tecmalik <abdulmalikojo2@gmail.com>
2025-05-10 18:23:46 +0100

    updating caculator server

--- awesomecalculatorprojectgo/models/calculator_test.go
+++ awesomecalculatorprojectgo/models/calculator_test.go
@@ -8,14 +8,19 @@ func Test_add(t *testing.T) {
 	}
 	tests := []struct {
 		name string
-		args string
+		args args
 		want string
 	}{
 		// TODO: Add test cases.
+		{
+			name: "simple addition ",
+			args: args{"5+5"},
+			want: "5",
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := add(tt.args); got != tt.want {
+			if got := calculator(tt.args.expression); got != tt.want {
 				t.Errorf("add() = %v, want %v", got, tt.want)
 			}
 		})
