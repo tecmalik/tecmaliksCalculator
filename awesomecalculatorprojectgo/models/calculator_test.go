ed8b93228224555fc619ea50ed3092d1baa6b643
tecmalik <abdulmalikojo2@gmail.com>
2025-05-11 12:24:06 +0100

    updating calculator backend

--- awesomecalculatorprojectgo/models/calculator_test.go
+++ awesomecalculatorprojectgo/models/calculator_test.go
@@ -20,7 +20,7 @@ func Test_add(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := calculator(tt.args.expression); got != tt.want {
+			if got := calculate(tt.args.expression); got != tt.want {
 				t.Errorf("add() = %v, want %v", got, tt.want)
 			}
 		})
