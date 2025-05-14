2d4500b56886123c505978596f19035160ed65b7
tecmalik <abdulmalikojo2@gmail.com>
2025-05-14 21:55:50 +0100

    updating calcator style

--- frontEnd/calculator/src/component/Calculator.jsx
+++ frontEnd/calculator/src/component/Calculator.jsx
@@ -10,8 +10,8 @@ const Calculator=()=>{
     const [value, setValue] = useState('0');
 
 
-    const handleAddNumber = (e,number) =>{
-        e.preventDefault();
+    const handleAddNumber = (number) =>{
+        
         console.log("current value"+ value)
         if (value === '0'){
             console.log(value)
@@ -22,10 +22,18 @@ const Calculator=()=>{
             
     };
 
+    const handleClearNumber = ()=>{
+        setValue('0');
+    }
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+    }
+
 
     return(
         <div id ={style.wrapper}>
-            <form className = {style.calculatorBody}>
+            <form className = {style.calculatorBody} onSubmit={handleSubmit}>
                 <div className = {style.display}>
                     <h1>{value}</h1>
                 </div>
@@ -33,26 +41,27 @@ const Calculator=()=>{
                     <div className = {style.firstRow}>
                         <CustomButton onClick = {() => handleAddNumber('1')} text ='1' />
                         <CustomButton onClick = {() => handleAddNumber('2')} text ='2'/>
-                        <CustomButton text ='3'/>
-                        <CustomButton text ='+'/>
+                        <CustomButton onClick = {() => handleAddNumber('3')}  text ='3'/>
+                        <CustomButton onClick = {() => handleAddNumber('+')}  text ='+'/>
                     </div>
                     <div className={style.secondRow}>
-                        <CustomButton text ='4'/>
-                        <CustomButton text ='5'/>
-                        <CustomButton text ='6'/>
-                        <CustomButton text ='-'/>
+                        <CustomButton onClick = {() => handleAddNumber('4')}  text ='4'/>
+                        <CustomButton onClick = {() => handleAddNumber('5')}  text ='5'/>
+                        <CustomButton onClick = {() => handleAddNumber('6')}  text ='6'/>
+                        <CustomButton onClick = {() => handleAddNumber('-')}  text ='-'/>
                     </div>
                     <div className={style.thirdRow}>
-                        <CustomButton text ='7'/>
-                        <CustomButton text ='8'/>
-                        <CustomButton text ='9'/>
-                        <CustomButton text ='*'/>
+                        <CustomButton onClick = {() => handleAddNumber('7')}  text ='7'/>
+                        <CustomButton onClick = {() => handleAddNumber('8')}  text ='8'/>
+                        <CustomButton onClick = {() => handleAddNumber('9')}  text ='9'/>
+                        <CustomButton onClick = {() => handleAddNumber('*')}  text ='*'/>
                     </div>
                     <div className={style.forthRow}>
-                        <CustomButton text ='%'/>
-                        <CustomButton text ='0'/>
-                        <CustomButton text ='/'/>
-                        <CustomButton text ='C'/>
+                        <CustomButton onClick = {() => handleAddNumber('%')}  text ='%'/>
+                        <CustomButton onClick = {() => handleAddNumber('0')}  text ='0'/>
+                        <CustomButton onClick = {() => handleAddNumber('/')}  text ='/'/>
+                        <button className ={style.cancel} onClick = {() => handleClearNumber()} >c</button>
+                        
                     </div>
                     <div className={style.lastRow}>
                     
